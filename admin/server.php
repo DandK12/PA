a7b5b482fbffb6b972379b751b2b4a6b450ebaf8
DandK12 <dandikurniawan1234@gmail.com>
2023-06-12 11:04:19 +0700

    70% Progress adding responsive tables

--- admin/server.php
+++ admin/server.php
@@ -1,40 +1,55 @@
 <?php
 
-    include "../koneksi.php";
+include "../koneksi.php";
 
-    $conn = connectToDatabase();
+$conn = connectToDatabase();
 
-    $sql  =  mysqli_query ($conn, "select * from teknisi ORDER BY id DESC LIMIT 1");
+$sql = mysqli_query($conn, "select * from teknisi ORDER BY id DESC LIMIT 1");
 ?>
 
-<table class="table">
-			<thead>
-				<th>Teknisi</th>
-				<th>Tanggal</th>
-				<th>Mitra</th>
-				<th>No Tiket</th>
-				<th>No Internet</th>
-				<th>Pekerjaan</th>
-			</thead>
-<?php 
-	while($row = mysqli_fetch_array($sql)){
+<div class="table-responsive">
+	<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+		<thead>
+			<th>Teknisi</th>
+			<th>Tanggal</th>
+			<th>Mitra</th>
+			<th>No Tiket</th>
+			<th>No Internet</th>
+			<th>Pekerjaan</th>
+		</thead>
+		<?php
+		while ($row = mysqli_fetch_array($sql)) {
 
-		$teknisi = $row['teknisi'];
-		$tanggal = $row['tanggal'];
-		$mitra = $row['mitra'];
-		$notiket = $row['notiket'];
-		$nointernet = $row['nointernet'];
-		$pekerjaan = $row['pekerjaan'];
- ?>
+			$teknisi = $row['teknisi'];
+			$tanggal = $row['tanggal'];
+			$mitra = $row['mitra'];
+			$notiket = $row['notiket'];
+			$nointernet = $row['nointernet'];
+			$pekerjaan = $row['pekerjaan'];
+			?>
 			<tbody>
 				<tr>
-					<td><?php echo $teknisi; ?></td>
-					<td><?php echo date('d-M-Y'); ?></td>
-					<td><?php echo $mitra; ?></td>
-					<td><?php echo $notiket; ?></td>
-					<td><?php echo $nointernet; ?></td>
-					<td><?php echo $pekerjaan; ?></td>
+					<td>
+						<?php echo $teknisi; ?>
+					</td>
+					<td>
+						<?php echo date('d-M-Y'); ?>
+					</td>
+					<td>
+						<?php echo $mitra; ?>
+					</td>
+					<td>
+						<?php echo $notiket; ?>
+					</td>
+					<td>
+						<?php echo $nointernet; ?>
+					</td>
+					<td>
+						<?php echo $pekerjaan; ?>
+					</td>
 				</tr>
 			</tbody>
-<?php 	} ?>
-		</table>
+		<?php } ?>
+	</table>
+
+</div>
--- admin/tables.php
+++ admin/tables.php
@@ -87,11 +87,10 @@
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
                     <!-- Sidebar Toggle (Topbar) -->
-                    <form class="form-inline">
-                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                            <i class="fa fa-bars"></i>
-                        </button>
-                    </form>
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
@@ -101,13 +100,13 @@
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                                <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
+                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="login.html" data-toggle="modal" data-target="#logoutModal">
+                                <a class="dropdown-item" href="login.php" data-toggle="modal"
+                                    data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
                                 </a>
@@ -202,7 +201,7 @@
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-danger" href="login.html">Logout</a>
+                    <a class="btn btn-danger" href="login.php">Logout</a>
                 </div>
             </div>
         </div>
